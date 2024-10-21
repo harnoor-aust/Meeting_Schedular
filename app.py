1cc5c715b2457ebb5476f38b6b7981059cad1f1a
Harnoor Bagga <harnoorbagga@Harnoors-MacBook-Pro.local>
2024-10-20 21:44:33 -0600

    modified port again

--- app.py
+++ app.py
@@ -113,4 +113,4 @@ def delete_event(event_id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5001)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
